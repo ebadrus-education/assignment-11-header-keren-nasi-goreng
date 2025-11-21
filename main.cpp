9c6f8a04ef074ef4ac9a0aeec6825536fd336e92
nanda-cpp <xeyvielivcn@gmail.com>
2025-11-21 13:36:40 +0700

    add deadline

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+void menuUtama() {
+}
 int main() {
     system("chcp 65001 >nul"); // enable UTF-8 untuk emoji & unicode
 
@@ -16,10 +18,37 @@ int main() {
 
     // cute mascot
     cout << "૮₍ ´ ꒳ `₎ა  ♡  Selamat Datang di Sistem Informasi Perpustakaan\n";
-    cout << "1. login pengguna\n";
-    cout << "2. daftar anggota baru\n";
-    cout << "3. pilih buku favoritmu hari ini! 📚🌸\n\n";
-    cout << "keluar untuk meninggalkan blossom library 🌷\n\n";
+
+    int pilihan;
+
+
+    do {
+        cout << "\nmenu utama\n";
+        cout << "1. cari buku\n";
+        cout << "2. pinjam buku\n";
+        cout << "3. kembalikan buku\n";
+        cout << "4. keluar\n";
+        cout << "masukkan pilihan anda: ";
+        cin >> pilihan;
+        switch (pilihan) {
+            case 1:
+                cout << "fungsi cari buku belum tersedia.\n";
+                break;
+            case 2:
+                cout << "fungsi pinjam buku belum tersedia.\n";
+                break;
+            case 3:
+                cout << "fungsi kembalikan buku belum tersedia.\n";
+                break;
+            case 4:
+                cout << "terima kasih telah menggunakan sistem kami! ૮₍ ´ ꒳ `₎ა\n";
+                break;
+            default:
+        
+            cout << "pilihan tidak valid, silakan coba lagi.\n";
+        
+        }
+    } while (pilihan != 4);
     cout << "\033[0m"; // reset warna
 
 
